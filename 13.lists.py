30b63dc419592fa68b62b622f04537cb01e6f7f8
Bilgenur Kara <71135791+bilgeekara@users.noreply.github.com>
2021-05-16 15:56:00 +0300

    Update 13.lists.py

--- 13.lists.py
+++ 13.lists.py
@@ -1,6 +1,6 @@
-# message = "Merhaba ben Ömer Faruk"
+# message = "Merhaba ben Bilge Kara"
 # print(message.split(' '))
-# message_list = ['Merhaba', 'ben', 'Ömer', 'Faruk']
+# message_list = ['Merhaba', 'ben', 'Bilge', 'Kara']
 # print(' '.join(message_list))
 # print(message_list[1])
 # liste = [1,2,5,3,8]
@@ -9,8 +9,8 @@
 # print(list(liste1))
 # liste1 = list(liste1)
 # print(type(liste1))
-liste = [12,'Faruk',9,8,True]
-liste2 = [[9,"Ömer",9.99,False],[9.9,85]]
+liste = [12,'Kara',9,8,True]
+liste2 = [[9,"Bilge",9.99,False],[9.9,85]]
 # my_list = liste + liste2
 # print(my_list)
 # print(len(my_list))
